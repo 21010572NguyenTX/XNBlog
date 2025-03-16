8b1d7c991d91e0ef9d1d3631a9f884c858c87716
Xuan Nguyen <nguyen03092003@gmail.com>
2025-03-16 15:02:46 +0700

    update smth netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: node({
-    mode: 'standalone'
+  adapter: netlify({
+    // These are the default options
+    dist: new URL('./dist/', import.meta.url),
   }),
   vite: {
     // Sử dụng 'VITE_' làm prefix thay vì để trống
